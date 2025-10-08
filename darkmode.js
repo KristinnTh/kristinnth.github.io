8b8287aada6caeb1316496f53398f1b7cff8c7f2
Ktheoret <Kristin.Theoret@gmail.com>
2025-10-08 12:13:31 -0400

    Apply Biome formatting to all project files

--- darkmode.js
+++ darkmode.js
@@ -1,21 +1,21 @@
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-}
+	document.body.classList.add("darkmode");
+	localStorage.setItem("darkmode", "active");
+};
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
-}
+	document.body.classList.remove("darkmode");
+	localStorage.setItem("darkmode", null);
+};
 
-if(darkmode === "active") enableDarkmode()
+if (darkmode === "active") enableDarkmode();
 
 // when button clicked execute this code:
 themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    //if active = true than enableDarkmode else disableDarkmode
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+	darkmode = localStorage.getItem("darkmode");
+	//if active = true than enableDarkmode else disableDarkmode
+	darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
